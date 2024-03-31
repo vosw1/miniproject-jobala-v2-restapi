19301d614df99b8f9ffc64859417962721df2821
ChanHyoek Park <1625787@donga.ac.kr>
2024-03-31 22:57:21 +0900

    스크랩 레스트 진행중

--- src/main/java/com/example/jobala/scrap/ScrapController.java
+++ src/main/java/com/example/jobala/scrap/ScrapController.java
@@ -1,61 +1,56 @@
 package com.example.jobala.scrap;
 
+import com.example.jobala._core.utill.ApiUtil;
 import com.example.jobala._user.User;
-import com.example.jobala.jobopen.Jobopen;
 import com.example.jobala.jobopen.JobopenResponse;
-import com.example.jobala.resume.Resume;
 import com.example.jobala.resume.ResumeResponse;
-import com.example.jobala.resume.ResumeService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ScrapController {
     private final HttpSession session;
     private final ScrapService scrapService;
 
     //기업의 스크랩 목록
-    @GetMapping("/comp/scrapForm")
-    public String compScrapForm(HttpServletRequest req) {
+    @GetMapping("/comp/scrap")
+    public ResponseEntity<?> compScrapForm() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        List<ResumeResponse.ScrapDTO> respDTO =  scrapService.scrapResumeBycomp(sessionUser.getId());
-        req.setAttribute("resumeList", respDTO);
-        return "comp/_myPage/scrapForm";
+        List<ResumeResponse.ScrapDTO> respDTO = scrapService.scrapResumeBycomp(sessionUser.getId());
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     //기업 스크랩
     @PostMapping("/comp/scrap")
-    public String scrapResume(ScrapRequest.CompScrapDTO reqDTO) {
+    public ResponseEntity<?> scrapResume(@RequestBody ScrapRequest.CompScrapDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        // 스크랩 했는지 확인 (null -> Scrap 안함, not null -> Scrap 함)
-        scrapService.scrapByComp(reqDTO, sessionUser);
-        return "redirect:/guest/resume/" + reqDTO.getResumeId();
+        Scrap scrap = scrapService.scrapByComp(reqDTO, sessionUser);
+        return ResponseEntity.ok(new ApiUtil<>(scrap));
     }
 
     // 개인의 스크랩 목록
-    @GetMapping("/guest/scrapForm")
-    public String guestScrapForm(HttpServletRequest req) {
+    @GetMapping("/guest/scrap")
+    public ResponseEntity<?> guestScrapForm() {
         User sessionUser = (User) session.getAttribute("sessionUser");
         List<JobopenResponse.ScrapDTO> respDTO = scrapService.scrapJobopenByGuest(sessionUser.getId());
-        req.setAttribute("jobopenList", respDTO);
-
-        return "guest/_myPage/scrapForm";
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     //개인 스크랩
     @PostMapping("/guest/scrap")
-    public String scrapJobopen(ScrapRequest.GuestScrap reqDTO) {
+    public ResponseEntity<?> scrapJobopen(@RequestBody ScrapRequest.GuestScrap reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        // 스크랩 했는지 확인 (null -> Scrap 안함, not null -> Scrap 함)
-        scrapService.scrapByGuest(reqDTO, sessionUser);
-        return "redirect:/comp/jobopen/" + reqDTO.getJobopenId();
+        Scrap scrap = scrapService.scrapByGuest(reqDTO, sessionUser);
+        return ResponseEntity.ok(new ApiUtil<>(scrap));
     }
 
 }
