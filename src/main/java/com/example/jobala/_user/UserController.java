ff5839492a891ca038a4c36b7c7be4299535fda8
vosw1 <vosw1@naver.com>
2024-03-31 20:33:09 +0900

    게시판 restAPI로 전환 - 상세보기 오류 해결

--- src/main/java/com/example/jobala/_user/UserController.java
+++ src/main/java/com/example/jobala/_user/UserController.java
@@ -6,14 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
