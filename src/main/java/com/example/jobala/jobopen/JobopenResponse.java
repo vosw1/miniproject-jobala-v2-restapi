7c6577bd8fc42a8854066859025dd348a4a54e34
kjh5848 <kjh5848@gmail.com>
2024-04-01 14:40:00 +0900

    오류 수정

--- src/main/java/com/example/jobala/jobopen/JobopenResponse.java
+++ src/main/java/com/example/jobala/jobopen/JobopenResponse.java
@@ -40,7 +40,6 @@ public class JobopenResponse {
     }
 
 
-    @AllArgsConstructor
     @Data
     public static class UpdateDTO {
         private Integer id;
@@ -69,8 +68,6 @@ public class JobopenResponse {
     }
 
 
-    @AllArgsConstructor
-    @NoArgsConstructor
     @Data
     public static class ListDTO {
         private Integer id;
