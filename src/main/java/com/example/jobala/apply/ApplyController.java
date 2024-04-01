de31ceffa7a4959bc0862d8c3ea25c458accd88e
test231212 <j910219@gmail.com>
2024-04-01 14:24:16 +0900

    ë©

--- src/main/java/com/example/jobala/apply/ApplyController.java
+++ src/main/java/com/example/jobala/apply/ApplyController.java
@@ -19,42 +19,31 @@ public class ApplyController {
 
     // 기업 및 개인 - 지원 상태 업데이트 기존 기업 개인으로 구분되어 있던 것을 합침
     @PostMapping("/api/applies/applyStatus/update")
-    public ResponseEntity<ApiUtil<String>> updateApplicationStatus(@RequestParam("applyId") Integer applyId, @RequestParam("status") String status) {
-        applyService.statusUpdate(applyId, status);
-        return ResponseEntity.ok(new ApiUtil("Status updated successfully"));
+    public ResponseEntity<ApiUtil<ApplyResponse.StatusUpdateDTO>> updateApplicationStatus(@RequestParam("applyId") Integer applyId, @RequestParam("status") String status) {
+        ApplyResponse.StatusUpdateDTO respDTO = applyService.statusUpdate(applyId, status);
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     // 개인 - 이력서 지원하기
     @PostMapping("/api/applies/applys")
-    public ResponseEntity<ApiUtil<String>> apply(@RequestBody ApplyRequest.ApplyRequestDTO reqDTO) {
+    public ResponseEntity<ApiUtil<ApplyResponse.ApplicationDTO>> apply(@RequestBody ApplyRequest.ApplyRequestDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        applyService.saveAfterApply(reqDTO, sessionUser);
-        return ResponseEntity.ok(new ApiUtil("Application submitted successfully"));
+        ApplyResponse.ApplicationDTO respDTO = applyService.saveAfterApply(reqDTO, sessionUser);
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     // 기업 및 개인 - 포지션 제안 현황보기
     @GetMapping("/api/applies/positionForm")
     public ResponseEntity<ApiUtil<?>> positionForm() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser.getRole() == 1) {
-            List<ApplyResponse.CompPositionDTO> respDTO = applyService.findApplyCompByUserId(sessionUser.getId());
-            return ResponseEntity.ok(new ApiUtil(respDTO));
-        } else {
-            List<ApplyResponse.GuestPositionDTO> respDTO = applyQueryRepository.findJopOpenByUserId(sessionUser.getId());
-            return ResponseEntity.ok(new ApiUtil(respDTO));
-        }
+        return applyService.findPositionFormByUserId(sessionUser);
     }
 
     // 기업 및 개인 - 이력서 지원 현황보기
     @GetMapping("/api/applies/applyForm")
     public ResponseEntity<ApiUtil<?>> applyForm() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser.getRole() == 1) {
-            List<ApplyResponse.CompApplyDTO> respDTO = applyQueryRepository.findByUserId(sessionUser.getId(), sessionUser.getRole());
-            return ResponseEntity.ok(new ApiUtil(respDTO));
-        } else {
-            List<ApplyResponse.GuestApplyDTO> respDTO = applyService.findApplyGuestByUserId(sessionUser.getId());
-            return ResponseEntity.ok(new ApiUtil(respDTO));
-        }
+        Object respDTO = applyService.findApplyGuestByUserId(sessionUser.getId(), sessionUser.getRole());
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 }
--- src/main/java/com/example/jobala/apply/ApplyService.java
+++ src/main/java/com/example/jobala/apply/ApplyService.java
@@ -1,21 +1,25 @@
 package com.example.jobala.apply;
 
 import com.example.jobala._core.errors.exception.Exception404;
+import com.example.jobala._core.utill.ApiUtil;
 import com.example.jobala._user.User;
 import com.example.jobala.jobopen.Jobopen;
 import com.example.jobala.jobopen.JobopenJPARepository;
 import com.example.jobala.resume.Resume;
 import com.example.jobala.resume.ResumeJPARepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ApplyService {
 
+    private final ApplyQueryRepository applyQueryRepository;
     private final ApplyJPARepository applyJPARepository;
     private final JobopenJPARepository jobopenJPARepository;
     private final ResumeJPARepository resumeJPARepository;
@@ -23,31 +27,55 @@ public class ApplyService {
 
     // 상태수정
     @Transactional
-    public void statusUpdate(Integer applyId, String status) {
+    public ApplyResponse.StatusUpdateDTO statusUpdate(Integer applyId, String status) throws Exception404 {
         Apply apply = applyJPARepository.findById(applyId)
                 .orElseThrow(() -> new Exception404("해당 정보를 찾을 수 없습니다."));
-
         apply.setState(status);
+        return new ApplyResponse.StatusUpdateDTO(true);
     }
 
-    // 지원후저장
+    // 지원 후 저장
     @Transactional
-    public void saveAfterApply(ApplyRequest.ApplyRequestDTO reqDTO, User sessionUser) {
+    public ApplyResponse.ApplicationDTO saveAfterApply(ApplyRequest.ApplyRequestDTO reqDTO, User sessionUser) throws Exception404 {
         Jobopen jobopen = jobopenJPARepository.findById(reqDTO.getJobopenId())
-                .orElseThrow(() -> new Exception404("공고를 찾을 수 없습니다"));
+                .orElseThrow(() -> new Exception404("공고를 찾을 수 없습니다."));
         Resume resume = resumeJPARepository.findById(reqDTO.getResumeId())
-                .orElseThrow(() -> new Exception404("이력서를 찾을 수 없습니다"));
+                .orElseThrow(() -> new Exception404("이력서를 찾을 수 없습니다."));
 
         applyJPARepository.save(reqDTO.toEntity(resume, jobopen, sessionUser));
+        return new ApplyResponse.ApplicationDTO(true);
     }
 
+    // 기업 사용자
     public List<ApplyResponse.CompPositionDTO> findApplyCompByUserId(Integer id) {
         List<Apply> applyList = applyJPARepository.findByUserId(id);
-        return applyList.stream().map(apply -> new ApplyResponse.CompPositionDTO(apply)).toList();
+        return applyList.stream()
+                .map(ApplyResponse.CompPositionDTO::new)
+                .collect(Collectors.toList());
     }
 
-    public List<ApplyResponse.GuestApplyDTO> findApplyGuestByUserId(Integer id) {
+    // 개인 사용자
+    public List<ApplyResponse.GuestPositionDTO> findJobOpenByUserId(Integer id) {
         List<Apply> applyList = applyJPARepository.findByUserId(id);
-        return applyList.stream().map(apply -> new ApplyResponse.GuestApplyDTO(apply)).toList();
+        return applyList.stream()
+                .map(ApplyResponse.GuestPositionDTO::new)
+                .collect(Collectors.toList());
     }
-}
+
+    // 사용자 역할에 따른 통합 조회 및 변환 로직
+    public ResponseEntity<ApiUtil<?>> findPositionFormByUserId(User sessionUser) {
+        if (sessionUser.getRole() == 1) {
+            List<ApplyResponse.CompPositionDTO> respDTO = findApplyCompByUserId(sessionUser.getId());
+            return ResponseEntity.ok(new ApiUtil<>(respDTO));
+        } else {
+            List<ApplyResponse.GuestPositionDTO> respDTO = findJobOpenByUserId(sessionUser.getId());
+            return ResponseEntity.ok(new ApiUtil<>(respDTO));
+        }
+    }
+
+        public Object findApplyGuestByUserId (Integer id, Integer role){
+
+            List<Apply> applyList = applyJPARepository.findByUserId(id);
+            return applyList.stream().map(apply -> new ApplyResponse.GuestApplyDTO(apply)).toList();
+        }
+    }
