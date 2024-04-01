863edec73ef97b4a5386c18083375e7806e62e9d
Pakchan <1625787@donga.ac.kr>
2024-04-01 17:58:46 +0900

    수정

--- src/main/java/com/example/jobala/jobopen/JobopenController.java
+++ src/main/java/com/example/jobala/jobopen/JobopenController.java
@@ -39,7 +39,7 @@ public class JobopenController {
 
     //공고 수정
     @PutMapping("/api/comp/jobopen/{id}")  // 주소 수정 필요
-    public ResponseEntity<?> update(@PathVariable Integer id, JobopenRequest.UpdateDTO reqDTO) {
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody JobopenRequest.UpdateDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         JobopenResponse.UpdateDTO respDTO = jobopenService.jobopenUpdate(id, sessionUser, reqDTO);
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
