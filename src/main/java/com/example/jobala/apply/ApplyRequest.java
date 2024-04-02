3f127acf123c6d0c58a8f20d761f9124673832dd
test231212 <j910219@gmail.com>
2024-04-02 14:15:29 +0900

    aop 완료

--- src/main/java/com/example/jobala/apply/ApplyRequest.java
+++ src/main/java/com/example/jobala/apply/ApplyRequest.java
@@ -3,6 +3,7 @@ package com.example.jobala.apply;
 import com.example.jobala._user.User;
 import com.example.jobala.jobopen.Jobopen;
 import com.example.jobala.resume.Resume;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +14,18 @@ public class ApplyRequest {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class ApplyStatusUpdateRequestDTO {
+        @NotEmpty
         private Integer applyId;
+        @NotEmpty
         private String status;
     }
 
     @Data
     @AllArgsConstructor
     public static class ApplyRequestDTO {
+        @NotEmpty
         private Integer resumeId;
+        @NotEmpty
         private Integer jobopenId;
 
         public Apply toEntity(Resume resume, Jobopen jobopen, User sessionUser) {
